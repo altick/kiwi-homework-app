7b56aa11456428cab8b945c08754abc9f71a453a
Petr Altman <altick87@hotmail.com>
2018-08-07 16:01:51 +0200

    Integrated with UI kit NativeBase

--- App.js
+++ App.js
@@ -1,13 +1,34 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
 import MainContext from './src/MainContext';
 import MainScreenWithContext from './src/MainScreen';
+import { Font } from 'expo';
+import { Text } from 'native-base';
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      fontLoaded: false
+    };
+  }
+  async componentWillMount() {
+      await Font.loadAsync({
+          'Roboto': require('./assets/Fonts/Roboto.ttf'),
+          'Roboto_medium': require('./assets/Fonts/Roboto_medium.ttf'),
+      });
+
+      this.setState({ fontLoaded: true });
+  }
+
   render() {
     return (
       <MainContext.Provider>
-        <MainScreenWithContext />
+        { this.state.fontLoaded 
+          ? <MainScreenWithContext />
+          : <Text>Loading...</Text>
+        }
       </MainContext.Provider>
     );
   }
--- src/MainScreen.js
+++ src/MainScreen.js
@@ -1,12 +1,21 @@
+// @flow
+
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import MainContext from './MainContext';
+import { Container, Header, Left, Body, Title, Segment, Right, Content, Footer, FooterTab, Button } from 'native-base';
 
+const MODE_PREDICT = 'predict';
+const MODE_EXPAND = 'expand';
 
 class MainScreen extends React.Component {
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            mode: MODE_PREDICT
+        };
     }
 
     async componentDidMount() {
@@ -18,15 +27,42 @@ class MainScreen extends React.Component {
     componentDidUpdate(prevProps, prevState) {
         // Previous SomeContext value is prevProps.someValue
         // New SomeContext value is this.props.someValue
-      }
+    }
+
+    setMode(mode) {
+        this.setState({ mode: mode });
+    }
 
     render() {
         // const { someValue, actions: { someAction } } = this.props;
+        const { mode } = this.state;
         
         return (
-            <View>
-                <Text>Hello</Text>
-            </View>
+            <Container>
+                <Header>
+                    <Left />
+                    <Body>
+                        <Title>Kiwi T9 App</Title>
+                        <Segment>
+                            <Button first active={ mode == MODE_PREDICT } onPress={ () => this.setMode(MODE_PREDICT) }><Text>Predictions</Text></Button>
+                            <Button last active={ mode == MODE_EXPAND } onPress={ () => this.setMode(MODE_EXPAND) }><Text>Expansions</Text></Button>
+                        </Segment>
+                    </Body>
+                    <Right />
+                </Header>
+                <Content>
+                    <Text>
+                        This is Content Section
+                    </Text>
+                </Content>
+                <Footer>
+                    <FooterTab>
+                        <Button full>
+                            <Text>Footer</Text>
+                        </Button>
+                    </FooterTab>
+                </Footer>
+            </Container>
         );
     }
 }
